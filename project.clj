515df64592a87a05b5db7bfc2e02bbcebcd372b7
Cliff Rodgers <kliph@users.noreply.github.com>
2016-06-19 21:26:21 -0500

    Bump clojure version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject cc.artifice/vijual "0.2.5"
+(defproject org.clojars.kliph/vijual "0.2.7"
   :description "A Graph Layout Library For Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha3"]])
